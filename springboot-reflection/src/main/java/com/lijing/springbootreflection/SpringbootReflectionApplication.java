9e459549a95e2814a8e1458f890be4c6de111a35
lijinghaitang <lijinghaitang@163.com>
2018-05-20 11:48:53 +0800

    reflection-start

---
+++ springboot-reflection/src/main/java/com/lijing/springbootreflection/SpringbootReflectionApplication.java
@@ -0,0 +1,12 @@
+package com.lijing.springbootreflection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReflectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReflectionApplication.class, args);
+	}
+}
